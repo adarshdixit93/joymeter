514cd42e32970e9623347a1416d5d20123338493
adarshdixit93 <143383476+adarshdixit93@users.noreply.github.com>
2024-01-01 18:04:19 +0530

    initial comit

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  isOpen = false;
+  toggleNav() {
+    this.isOpen = !this.isOpen;
+  }
 }
